eff92a4c0c5549f1677bcefd71b3894265a277e7
vladsergy <drylb924@gmail.com>
2020-12-19 19:47:15 +0200

    clean up

--- src/tree.js
+++ src/tree.js
@@ -8,13 +8,6 @@ import {
 } from '@hexlet/immutable-fs-trees';
 
 // BEGIN (write your solution here)
-// const tree = mkdir('my documents', [
-//   mkfile('avatar.jpg', { size: 100 }),
-//   mkfile('passport.jpg', { size: 200 }),
-//   mkfile('family.jpg', { size: 150 }),
-//   mkfile('addresses', { size: 125 }),
-//   mkdir('presentations'),
-// ]);
 
 // VERSION #1
 
@@ -37,6 +30,8 @@ const compressImages = (tree) => {
     });
   return mkdir(newName, newChildren, globalMeta);
 };
+
+// VERSION #2
 
 const compressImages1 = (tree) => {
   const children = getChildren(tree);
