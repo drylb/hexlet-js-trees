1569de20756eb38dd1794362fdbe262ed4fb3799
vladsergy <drylb924@gmail.com>
2020-12-21 02:57:03 +0200

    clean up

--- src/du.js
+++ src/du.js
@@ -2,7 +2,7 @@
 
 import _ from 'lodash';
 import {
-  isFile, getName, getMeta, getChildren, mkdir, mkfile,
+  isFile, getName, getMeta, getChildren,
 } from '@hexlet/immutable-fs-trees';
 
 // BEGIN (write your solution here)
